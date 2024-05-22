a0df5be36513ad51c89e14b536ab4e9887dcbf4b
Joe Thompson <jthompson7@blueorigin.com>
2024-05-22 10:26:51 -0700

    Change dbauth CORS policy and don't load additional nginx configs.

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -159,7 +159,7 @@ export const handler = async (event, context) => {
       attributes: {
         HttpOnly: true,
         Path: '/',
-        SameSite: 'Lax',
+        SameSite: 'Strict',
         Secure: process.env.NODE_ENV !== 'development',
 
         // If you need to allow other domains (besides the api side) access to
